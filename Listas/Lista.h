5839a1af198b63261f79fa9f650d82dec905375c
louis-br <46930119+louis-br@users.noreply.github.com>
2019-10-27 10:18:23 -0300

    Adicionar inserção na lista

--- Listas/Lista.h
+++ Listas/Lista.h
@@ -8,17 +8,35 @@ private:
 	class Elemento {
 	private:
 		Elemento<TE>* pProx;
+		Elemento<TE>* pElem;
 	public:
-		Elemento() {}
+		Elemento(TE* elemento) :
+			pProx(nullptr),
+			pElem(elemento) {}
 		~Elemento() {}
-		Elemento<TE*> getProximo() {}
+		Elemento<TE*> getProximo() { return pProx; }
+		Elemento<TE*> getElemento() { return pElem; }
+		void setProximo(Elemento<TE>* elemento) { pProx = elemento; }
 	};
 	Elemento<TL>* pPrimeiro;
 	Elemento<TL>* pUltimo;
 public:
-	Lista() {}
+	Lista() :
+		pPrimeiro(nullptr),
+		pUltimo(nullptr) {}
 	~Lista() {}
 	Elemento<TL>* getPrimeiro() { return pPrimeiro; }
 	Elemento<TL>* getUltimo() { return pUltimo; }
+	void Inserir(TL* elemento) {
+		Elemento<TL>* novo = new Elemento(elemento);
+		if (pPrimeiro == nullptr) {
+			pPrimeiro = novo;
+			pUltimo = novo;
+		}
+		else {
+			pUltimo->setProximo(novo);
+			pUltimo = novo;
+		}
+	}
 };
 
